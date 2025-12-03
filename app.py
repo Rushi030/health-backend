0fa6922fd04ab00d65a5036d41933a689d72dcf3
Rushi030 <rushikeshaj1@gmail.com>
2025-12-03 22:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -800,9 +800,8 @@ if __name__ == "__main__":
     
     # Run Flask app
 
-   if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=10000)
+
+    app.run(debug=True, host="127.0.0.1", port=5000)
 
 
 
